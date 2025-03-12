c6e1c7c454d085a0a34829c526eef12289f35639
BrunoMorback <bruno_morbackk@hotmail.com>
2025-03-12 11:40:39 -0300

    Opção de saida
    Incluso no sistema a opção de saída do usuário.

--- Cartorio EBAC.cpp
+++ Cartorio EBAC.cpp
@@ -126,6 +126,7 @@ int main()
 	  printf("\t1 - Registrar nomes\n");
 	  printf("\t2 - Consultar nomes\n");
 	  printf("\t3 - Deletar nomes\n\n");
+	  printf("\t4 - Sair do sistema\n\n");
 	  printf("Opção: "); // Fim do menu
 	
 	  scanf("%d", &opcao); // Armazenado escolha do usuario
@@ -145,6 +146,11 @@ int main()
 		case 3:
 		deletar();
 		break;
+		
+		case 4:
+	    printf("Obrigado por ultilizar o sistema!");
+	    return 0;
+	    break;
 		
 		default:
 		printf("Essa opção não esta disponivel!\n");
